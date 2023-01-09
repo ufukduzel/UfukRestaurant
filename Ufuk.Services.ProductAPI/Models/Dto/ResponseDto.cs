4ff0fff406ae2d0f3296ab1af46d591712c472fe
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-01-09 23:07:47 +0300

    15th step.

---
+++ Ufuk.Services.ProductAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Ufuk.Services.ProductAPI.Models.Dto
+{
+    //14th step.
+    public class ResponseDto
+    {
+        //15th step.
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessage { get; set; }
+    }
+}
--- Ufuk.Services.ProductAPI/Models/ProductDto.cs
+++ Ufuk.Services.ProductAPI/Models/ProductDto.cs
@@ -1,7 +1,16 @@
-﻿namespace Ufuk.Services.ProductAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ufuk.Services.ProductAPI.Models.Dtos //13th step.
 {
     //11th step.
     public class ProductDto
     {
+        //12th step.
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
