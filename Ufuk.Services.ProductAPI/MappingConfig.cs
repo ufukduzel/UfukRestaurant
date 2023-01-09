7ba71e205d4cbe039894a27a0bf98ff29dce67c0
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-01-09 23:20:41 +0300

    18th step.

---
+++ Ufuk.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Ufuk.Services.ProductAPI.Models.Dto;
+using Ufuk.Services.ProductAPI.Models.Dtos;
+
+namespace Ufuk.Services.ProductAPI
+{
+    //17th step.
+    public class MappingConfig
+    {
+        //18th step.
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
