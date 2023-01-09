4db3d08c0e7f634c9a693a8f2179e54083f26cce
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-01-09 23:12:58 +0300

    16th step.

--- Ufuk.Services.ProductAPI/Repository/IProductRepository.cs
+++ Ufuk.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace Ufuk.Services.ProductAPI.Repository
+﻿using Ufuk.Services.ProductAPI.Models.Dtos;
+
+namespace Ufuk.Services.ProductAPI.Repository
 {
     //10th step.
     public interface IProductRepository
     {
+        //16th step.
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
