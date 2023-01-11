251933dc1b005d464bccc563d661ce590847b678
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-01-11 23:28:51 +0300

    21th step.

--- Ufuk.Services.ProductAPI/Repository/ProductRepository.cs
+++ Ufuk.Services.ProductAPI/Repository/ProductRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace Ufuk.Services.ProductAPI.Repository
+﻿using AutoMapper;
+using Ufuk.Services.ProductAPI.DbContexts;
+using Ufuk.Services.ProductAPI.Models.Dtos;
+
+namespace Ufuk.Services.ProductAPI.Repository
 {
-    public class ProductRepository
+    //20th step.
+    public class ProductRepository : IProductRepository
     {
+        //21th step.
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+        public ProductRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> GetProductById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
