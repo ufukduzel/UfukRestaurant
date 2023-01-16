3b09bb399fefb21d0257a0ec070728b3f8b3e24a
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-01-17 00:26:02 +0300

    //53rd step.

--- Ufuk.Web/Services/IServices/IProductService.cs
+++ Ufuk.Web/Services/IServices/IProductService.cs
@@ -1,7 +1,15 @@
-﻿namespace Ufuk.Web.Services.IServices
+﻿using Ufuk.Web.Models;
+
+namespace Ufuk.Web.Services.IServices
 {
     //48th step.
     public interface IProductService
     {
+        //53rd step.
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(int id);
     }
 }
