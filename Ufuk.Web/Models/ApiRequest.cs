c9e2bead48287e242b4e81aca3eaddbf82216ca7
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-01-17 00:52:13 +0300

    55th step.

--- Ufuk.Web/Models/ApiRequest.cs
+++ Ufuk.Web/Models/ApiRequest.cs
@@ -1,6 +1,14 @@
-﻿namespace Ufuk.Web.Models
+﻿using static Ufuk.Web.SD;
+
+namespace Ufuk.Web.Models
 {
+    //54th step.
     public class ApiRequest
     {
+        //55th step.
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
     }
 }
