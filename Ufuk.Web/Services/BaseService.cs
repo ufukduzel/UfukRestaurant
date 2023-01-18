5dc97a5dae952916359b0f724d4472f28cadc2d6
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-01-18 23:04:19 +0300

    59th step.

--- Ufuk.Web/Services/BaseService.cs
+++ Ufuk.Web/Services/BaseService.cs
@@ -1,7 +1,22 @@
-﻿namespace Ufuk.Web.Services
+﻿using Ufuk.Web.Models;
+using Ufuk.Web.Services.IServices;
+
+namespace Ufuk.Web.Services
 {
     // 58th step.
-    public class BaseService
+    public class BaseService : IBaseService
     {
+        // 59th step.
+        public ResponseDto responseModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
