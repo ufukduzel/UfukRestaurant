319139a46bda2f4a1933d218988d9ea7f5454d86
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-01-18 23:01:48 +0300

    57th step.

--- Ufuk.Web/Services/IServices/IBaseService.cs
+++ Ufuk.Web/Services/IServices/IBaseService.cs
@@ -1,7 +1,12 @@
-﻿namespace Ufuk.Web.Services.IServices
+﻿using Ufuk.Web.Models;
+
+namespace Ufuk.Web.Services.IServices
 {
     // 56th step.
-    public interface IBaseService
+    public interface IBaseService : IDisposable
     {
+        // 57th step.
+        ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
     }
 }
