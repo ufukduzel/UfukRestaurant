b91d6739470029b5fd3a84821c1c600373569e57
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-01-23 23:44:32 +0300

    95th step.

---
+++ Ufuk.Web/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ufuk.Web.Models;
+using Ufuk.Web.Services.IServices;
+
+namespace Ufuk.Web.Controllers
+{
+    // 92nd step.
+    public class ProductController : Controller
+    {
+        // 93rd step.
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // 94th step.
+        public async Task<IActionResult> ProductIndex() // 95th step.
+        {
+            List<ProductDto> list = new List<ProductDto>();
+            return View();
+        }
+    }
+}
