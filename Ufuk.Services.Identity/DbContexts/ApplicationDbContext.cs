1bd8f2be5f10061a3b924eb42f77c9b88010d800
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-02-07 23:59:17 +0300

    130th step.

--- Ufuk.Services.Identity/DbContexts/ApplicationDbContext.cs
+++ Ufuk.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Ufuk.Services.Identity.Models;
 
 namespace Ufuk.Services.Identity.DbContexts
 {
     // 128th step.
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> // 130th step.
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
