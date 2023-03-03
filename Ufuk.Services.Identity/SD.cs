ccda96338c2ae4535823ba36fad1d09900a182e4
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-03-03 23:38:46 +0300

    134th step.

--- Ufuk.Services.Identity/SD.cs
+++ Ufuk.Services.Identity/SD.cs
@@ -9,11 +9,22 @@ namespace Ufuk.Services.Identity
         public const string Customer = "Customer";
 
         // 133rd step.
-        public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
-        {
-            new IdentityResources.OpenId(),
-            new IdentityResources.Email(),
-            new IdentityResources.Profile()
-        };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        // 134th step.
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("Ufuk", "Ufuk Server"),
+                new ApiScope(name: "read", displayName: "Read your data."),
+                new ApiScope(name: "write", displayName: "Write your data."),
+                new ApiScope(name: "delete", displayName: "Delete your data.")
+            };
     }
 }
