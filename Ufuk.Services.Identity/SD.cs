06c58c1dfe84a203ec36da67420f58107371cbfa
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-03-03 23:30:37 +0300

    133rd step.

--- Ufuk.Services.Identity/SD.cs
+++ Ufuk.Services.Identity/SD.cs
@@ -1,9 +1,19 @@
-﻿namespace Ufuk.Services.Identity
+﻿using Duende.IdentityServer.Models;
+
+namespace Ufuk.Services.Identity
 {
     public class SD // 131st step.
     {
         // 132nd step.
         public const string Admin = "Admin";
         public const string Customer = "Customer";
+
+        // 133rd step.
+        public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
     }
 }
